b28f19a4652ca4149f4079a129c255d9702a5125
captproton <carl@wdwhub.net>
2024-07-12 13:56:47 -0700

    CHANGE: added helpers in ApplicationController; used helpers in head and footer

--- app/helpers/bravura_template_normal/application_helper.rb
+++ app/helpers/bravura_template_normal/application_helper.rb
@@ -4,5 +4,17 @@
 module BravuraTemplateNormal
   # Helper methods for application views
   module ApplicationHelper
+    def favicon_url
+      general_settings = Settings::General.cached(Current.account.id)
+      if general_settings&.favicon&.attached?
+        rails_blob_url(general_settings.favicon)
+      else
+        asset_path('default_favicon.png')
+      end
+    end
+
+    def social_links
+      Settings::General.cached(Current.account.id)&.platform_links || {}
+    end
   end
 end
