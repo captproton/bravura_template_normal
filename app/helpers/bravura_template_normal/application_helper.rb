0e5db84341ddcec6cc8e847cc0e0a573f5ea80d5
captproton <carl@wdwhub.net>
2024-07-13 11:57:16 -0700

    CHANGE: mvp for spec/helpers/bravura_template_normal/application_helper_spec.rb with spec

--- app/helpers/bravura_template_normal/application_helper.rb
+++ app/helpers/bravura_template_normal/application_helper.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-# Provides helper methods for the BravuraTemplateNormal application
+# app/helpers/bravura_template_normal/application_helper.rb
+
+# Provides view helper methods that span across all public-facing pages for the BravuraTemplateNormal engine
 module BravuraTemplateNormal
-  # Helper methods for application views
+  # ApplicationHelper contains helper methods for use across the BravuraTemplateNormal engine.
+  # It includes methods for fetching favicon URLs and social platform links based on account settings.
   module ApplicationHelper
+    # Returns the URL for the favicon based on the current account's settings
+    # or a default favicon if none is attached.
     def favicon_url
-      general_settings = Settings::General.cached(Current.account.id)
+      general_settings = BravuraTemplateNormal::Settings::General.find_by(account_id: Current.account.id)
       if general_settings&.favicon&.attached?
         rails_blob_url(general_settings.favicon)
       else
@@ -13,8 +18,10 @@ module BravuraTemplateNormal
       end
     end
 
+    # Returns a hash of social platform links based on the current account's settings.
     def social_links
-      Settings::General.cached(Current.account.id)&.platform_links || {}
+      general_settings = BravuraTemplateNormal::Settings::General.find_by(account_id: Current.account.id)
+      general_settings&.platform_links || {}
     end
   end
 end
---
+++ spec/helpers/bravura_template_normal/application_helper_spec.rb
@@ -0,0 +1,66 @@
+# spec/helpers/bravura_template_normal/application_helper_spec.rb
+require 'rails_helper'
+
+module BravuraTemplateNormal
+  RSpec.describe ApplicationHelper, type: :helper do
+    let(:account) { double('Account', id: 1) }
+    let(:general_settings) { double('Settings::General', favicon: nil, platform_links: nil) }
+
+    before do
+      stub_const('Current', double(account:))
+
+      stub_const('BravuraTemplateNormal::Settings::General', Class.new do
+        def self.find_by(*)
+          # This method will be stubbed in each test
+        end
+      end)
+
+      allow(BravuraTemplateNormal::Settings::General).to receive(:find_by).and_return(general_settings)
+    end
+
+    describe '#favicon_url' do
+      context 'when favicon is attached' do
+        let(:favicon) { double('favicon', attached?: true) }
+
+        before do
+          allow(general_settings).to receive(:favicon).and_return(favicon)
+          allow(helper).to receive(:rails_blob_url).with(favicon).and_return('http://example.com/favicon.ico')
+        end
+
+        it 'returns the favicon url' do
+          expect(helper.favicon_url).to eq('http://example.com/favicon.ico')
+        end
+      end
+
+      context 'when favicon is not attached' do
+        before do
+          allow(helper).to receive(:asset_path).with('default_favicon.png').and_return('/assets/default_favicon.png')
+        end
+
+        it 'returns the default favicon path' do
+          expect(helper.favicon_url).to eq('/assets/default_favicon.png')
+        end
+      end
+    end
+
+    describe '#social_links' do
+      context 'when platform links are set' do
+        let(:platform_links) { { facebook: 'https://facebook.com/example', twitter: 'https://twitter.com/example' } }
+
+        before do
+          allow(general_settings).to receive(:platform_links).and_return(platform_links)
+        end
+
+        it 'returns the platform links from general settings' do
+          expect(helper.social_links).to eq(platform_links)
+        end
+      end
+
+      context 'when platform links are not set' do
+        it 'returns an empty hash' do
+          expect(helper.social_links).to eq({})
+        end
+      end
+    end
+  end
+end
